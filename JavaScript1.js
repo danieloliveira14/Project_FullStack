0c8706d865737843844fc49b9bd99583081c3341
Daniel Oliveira <daniel.oliveira@mobiauto.com.br>
2023-03-01 23:33:56 -0300

    Camada FrontEnd

---
+++ JavaScript1.js
@@ -0,0 +1,17 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+    var tel = document.getElementById("foneid");
+    var email = document.getElementById("emailid");
+ if (tel.value != "") {
+ alert('Obrigado sr(a) ' + nome.value + ' Os seus dados, Tel: '+ tel.value + ' E-mail:' + email.value + '. Foram enviados com Sucesso');
+ }
+ }
+ 
+ function Trocar() {
+ 
+    var nome = document.getElementById("nomeid");
+    var email = document.getElementById("emailid");
+    document.getElementById("emailid").value  = nome.value
+ }
+ 
